8f0b0a131a64c85c56edcd8050827cb37267970f
caoyongpeng <1533392605@qq.com>
2019-11-01 15:39:21 +0800

    练习

---
+++ demo/book/urls.py
@@ -0,0 +1,10 @@
+
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view()),
+    url(r'^books/$', views.BooksView.as_view()),
+    url(r'^books/(?P<pk>\d+)/$', views.BookView.as_view()),
+]
